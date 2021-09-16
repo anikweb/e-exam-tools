d0e9a25ef90e65daa0e68e3a9f0f1c1f61f41b13
anikweb <aniknandi.it@gmail.com>
2021-09-16 15:33:26 +0600

    dashboard integration,login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,77 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in | E-Exam Tools</title>
+    <link rel="stylesheet" href="{{ asset('backend/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- bootstrap 4 css  -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body class="bg-dark">
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-md-5 mx-auto my-auto">
+                <div class="card ">
+                    <div class="card-header bg-success text-center text-white">
+                        <div class="card-title">
+                            <img width="150px" src="{{ asset('backend/dist/img/rimt.png') }}" alt="rimt logo">
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="text-center">Sign In</h5>
+                        <form action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                @error('email')
+                                    <div class="text-danger">
+                                        @if ($message == 'These credentials do not match our records.')
+                                            <i class="fa fa-exclamation-circle"></i>
+                                            {{ $message }}
+                                        @endif
+                                    </div>
+                                @enderror
+                                <label for="">Email</label>
+                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                                @error('email')
+                                    <div class="text-danger">
+                                       @if ($message != 'These credentials do not match our records.')
+                                            <i class="fa fa-exclamation-circle"></i>
+                                            {{ $message }}
+                                       @endif
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" name="password" autocomplete="current-password" class="form-control @error('password') is-invalid @enderror">
+                                @error('password')
+                                    <div class="text-danger">
+                                        <i class="fa fa-exclamation-circle"></i>
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="remember_token" name="remember">
+                                <label for="remember_token">Remember me</label>
+                            </div>
+                            <div class="form-group text-center">
+                                <button class="btn btn-success" type="submit"><i class="fa fa-sign-in-alt"></i> Sign In</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+    <!-- jquery 3 js  -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <!-- popper js  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <!-- botstrap 4 js  -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
